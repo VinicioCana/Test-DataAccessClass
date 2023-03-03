f8a13e5b0d91a685318413db6bbbf863c955b199
VinicioCana <viniciocana@gmail.com>
2023-03-03 10:17:08 -0600

    se cambia la lógica del método ExecuteNonQuery para agregarle la lógica de la consulta anidada

--- dataAccess.cs
+++ dataAccess.cs
@@ -32,11 +32,14 @@ public class Test4
 
         /* Devuelve el número de filas afectadas en la ejecución del query.
         Inicializamos la variable rows a O y dentro del using la modificamos
-        en función del resusltado del query.*/
-        
-        public int ExecuteNonQuery(string query)
+        en función del resusltado del query.
+        Si el query contine un SELECT se crea una consulta anidada que permite hacer el conteo del select 
+        cuya respuesta se devuelve en el parámetro de salida rowCount*/
+
+        public int ExecuteNonQuery(string query, out int rowCount)
         {
             int rows = 0;
+            rowCount = 0;
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -44,26 +47,16 @@ public class Test4
 
                 SqlCommand command = new SqlCommand(query, connection);
                 rows = command.ExecuteNonQuery();
+
+                // Si la consulta contiene un SELECT, contamos el número de filas, dentro de una subconsulta 
+                if (nonQuery.StartsWith("SELECT", StringComparison.OrdinalIgnoreCase))
+                {
+                    command.CommandText = $"SELECT COUNT(*) FROM ({query}) AS M";
+                    rowCount = (int)command.ExecuteScalar();
+                }
             }
 
             return rows;
-        }
-
-        // Devuelve el número de filas que devolveria el select del query que recibe como parámetro de entrada.
-
-        public int GetRowCountSelect(string query)
-        {
-            int rowCount = 0;
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                SqlCommand command = new SqlCommand(query, connection);
-                rowCount = (int)command.ExecuteScalar();
-            }
-
-            return rowCount;
         }
     }
 }
